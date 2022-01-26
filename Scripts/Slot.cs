d82cf940a488e158f723de3ce11f52d331563d0a
cloudeven <cloudeven@gmail.com>
2022-01-26 16:41:21 +0900

    begin

---
+++ Scripts/Slot.cs
@@ -0,0 +1,18 @@
+/*
+ * Create: [Slot.cs] on Wed Jan 26 2022 오후 4:40:37
+ * Author: [cloudeven@gmail.com]
+ *
+ * Copyright (c) 2022 [noname]
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot
+{
+	public Slot()
+	{
+		
+	}
+}
---
+++ Scripts/SlotManager.cs
@@ -0,0 +1,15 @@
+/*
+ * Create: [SlotManager.cs] on Wed Jan 26 2022 오후 4:40:32
+ * Author: [cloudeven@gmail.com]
+ *
+ * Copyright (c) 2022 [noname]
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlotManager
+{
+}
